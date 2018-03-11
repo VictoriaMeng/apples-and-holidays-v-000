95534c3be2e06b0c1186193cf1658e7c039ea0a2
Victoria Meng <nights1ststar@protonmail.com>
2018-03-11 00:08:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,11 +57,12 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     season_text = season.to_s.capitalize
-    holidays.each do |holiday|
-      holiday_text = season.to_s.capitalize
-      
-    end
     puts "#{season_text}:"
+    holidays.each do |holiday|
+      holiday_text = holiday.to_s.capitalize
+
+    end
+
   end
 end
 
